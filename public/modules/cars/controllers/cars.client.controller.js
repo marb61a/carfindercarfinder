1c67a9604a01188e4449e1c5411f0ebb55a0ebdf
marb <marb61a@gmail.com>
2015-08-11 08:21:52 +0000

    updated controller and edit view

--- public/modules/cars/controllers/cars.client.controller.js
+++ public/modules/cars/controllers/cars.client.controller.js
@@ -7,20 +7,34 @@ angular.module('cars').controller('CarsController', ['$scope', '$stateParams', '
 
 		// Create new Car
 		$scope.create = function() {
-			
 			// Create new Car object
 			var car = new Cars({
 				title: this.title,
-				content: this.content
+				make: this.make,
+				model: this.model,
+				type: this.type,
+				year: this.year,
+				price: this.price,
+				description: this.description,
+				imageurl: this.imageurl,
+				state: this.state,
+				contact_email: this.contact_email
 			});
 
 			// Redirect after save
 			car.$save(function(response) {
 				$location.path('articles/' + response._id);
-
 				// Clear form fields
 				$scope.title = '';
-				$scope.content = '';
+				$scope.make = '';
+				$scope.model = '';
+				$scope.type = '';
+				$scope.year= '';
+				$scope.price= '';
+				$scope.imageurl = '';
+				$scope.description = '';
+				$scope.state = '';
+				$scope.contact_email = '';
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
@@ -28,7 +42,6 @@ angular.module('cars').controller('CarsController', ['$scope', '$stateParams', '
 
 		// Remove existing Car
 		$scope.remove = function(car) {
-			
 			if (car) {
 				car.$remove();
 				for (var i in $scope.cars) {
@@ -43,11 +56,9 @@ angular.module('cars').controller('CarsController', ['$scope', '$stateParams', '
 			}
 		};
 
-		// Update existing Car
+		// Update an existing Car
 		$scope.update = function() {
-			
 			var car = $scope.car;
-
 			car.$update(function() {
 				$location.path('cars/' + car._id);
 			}, function(errorResponse) {
@@ -60,7 +71,7 @@ angular.module('cars').controller('CarsController', ['$scope', '$stateParams', '
 			$scope.cars = Cars.query();
 		};
 
-		// Find existing Car
+		// Find an sexisting Car
 		$scope.findOne = function() {
 			$scope.car = Cars.get({
 				carId: $stateParams.carId
